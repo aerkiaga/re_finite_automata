4c0f9bcf5883fb29deebe83604e233f0bba5fa29
Aritz Erkiaga <aerkiaga3@gmail.com>
2026-03-15 19:29:05 +0100

    Add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.0] - 2026-03-15
+
+### Added
+
+- Deterministic and nondeterministic finite-state automata types.
+- Methods for inspecting automata.
+- Methods for running automata on byte iterators.
+- `from_range`, `append`, `combine` and `invert` for creating `Nfa` objects.
+- Operator overrides for the above.
+- Various tests and benchmarks.
--- benches/bench.rs
+++ benches/bench.rs
@@ -27,10 +27,20 @@ pub fn bench_nfa_4_match(c: &mut Criterion) {
     });
 }
 
+pub fn bench_nfa_either_match(c: &mut Criterion) {
+    let input: Vec<u8> = black_box((0..1000).map(|_| 0).collect());
+    let nfa = Nfa::from_range(0..=1);
+    let nfa = nfa.clone() | nfa;
+    c.bench_function("NFA either match", |b| {
+        b.iter(|| black_box(nfa.run(input.iter().copied())))
+    });
+}
+
 criterion_group!(
     benches,
     bench_nfa_no_match,
     bench_nfa_char_match,
-    bench_nfa_4_match
+    bench_nfa_4_match,
+    bench_nfa_either_match
 );
 criterion_main!(benches);
